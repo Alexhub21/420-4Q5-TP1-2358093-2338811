561985d421b22385d11f853ac9917f535ef9089b
Marcoo-lim <2338811@cegeplimoilou.ca>
2026-03-04 18:36:32 -0500

    fin class SousReseau + test ex 3

--- Exercice3.ps1
+++ Exercice3.ps1
@@ -27,18 +27,17 @@ if($PositionHote -ge ($capacite - 1)){
 
 #Convertir IP réseau en entier
  $tabOctet = $this.Adresse.Split('.')
- [int]$base = 0
+ [uint32]$base = 0
  $i = 0
  while ($i -lt 4) {
-    $base = ($base * 256) + [int]$tabOctet[$i]
+    $base = ($base * 256) + [uint32]$tabOctet[$i]
     $i ++
  }
  #Ajouter PositionHote
 
- [int]$nouveauIP = $base + $PositionHote
+ [uint32]$nouveauIP = $base + $PositionHote
 
 #Retourner nouvelle IP
-
 return $nouveauIP
 }
 
@@ -59,28 +58,23 @@ class VLSMCalculateur{
         $this.MasqueDepart= $MasqueDepart
         $this.Hotes= $Hotes
     
-
  #validation IPV4 avec le regex
     if($this.AdresseDepart -notmatch '^((25[0-5]|2[0-4]\d|1?\d?\d)(\.(25[0-5]|2[0-4]\d|1?\d?\d)){3})$'){
         throw"•	L'adresse IP de base n'est pas au format IPv4 valide."
 
     }
-
 #validation du masque
     if (($this.MasqueDepart -lt 0 -or $this.MasqueDepart -gt 30)) {
         throw"Masque invalide : $($this.MasqueDepart)"
     }
-
 #validation des hotes
     if(-not $this.Hotes -or $this.Hotes.Count -eq 0) {
         throw"La liste d'hôtes est vide"
     }
-
     $i = 0
     while ($i -lt $this.Hotes.Count) {
         if ($this.Hotes[$i] -le 0) {
             throw "Nombre d'Hôte invalide : $($this.Hotes[$i])"
-
         }
           $i++
     }
@@ -98,16 +92,12 @@ class VLSMCalculateur{
         $SommeAdressesTotal = $SommeAdressesTotal + $AdressesTotales
 
          Write-Debug "Validation: Hôtes :$NbHote , MasqueFin : $MasqueFin , TotalAdresses : $AdressesTotales , Somme : $SommeAdressesTotal" 
-
             $i++
     }
-    
         if ($SommeAdressesTotal -gt $capacite){
             throw"la somme des sous-réseau ($SommeAdressesTotal) dépasse la capacité ($capacite)"
         }
-      
          Write-Debug "Constructeur VLSMCalculateur() - validation OK"
-
     }
 
 
@@ -121,7 +111,6 @@ class VLSMCalculateur{
     if($Hotes -le 0){
         throw "Le nombre d'hôtes doit être supérieur à 0"
     }
-
        $bitsHotes = [math]::Ceiling([math]::Log($Hotes + 2, 2))
         $masqueSousReseau = 32 - $bitsHotes
 
@@ -138,4 +127,57 @@ class VLSMCalculateur{
 
     
     }
+
+
+   [SousReseau[]] Calculer(){
+
+     Write-Debug "Calculer() - début"
+
+    $sousReseaux = @()
+    #trie des hotes par ordre decroissant
+    $HotesTries = $this.Hotes | Sort-Object -Descending
+
+    $tabOctet = $this.AdresseDepart.Split('.')
+    [uint32]$base = 0
+    $i = 0
+    while ($i -lt 4) {
+        $base = ($base * 256) + [uint32]$tabOctet[$i]
+        $i ++
+    }
+    #le calcul séquencielles
+    for ($i = 0;$i -lt $HotesTries.Count; $i++){
+        $NbHote =$HotesTries[$i]
+         [int]$MasqueFin = $this.GetMasque($HotesTries[$i])
+
+        # -shr et -band trouver a partir de recherche sur internet pour convertir entier en adresse ip
+
+        $o1 = [int](($base -shr 24) -band 255)
+        $o2 = [int](($base -shr 16) -band 255)
+        $o3 = [int](($base -shr 8) -band 255)
+        $o4 = [int]($base -band 255)
+
+        $AdresseCourante = "$o1.$o2.$o3.$o4"
+
+         # Créer l'objet SousReseau et l'ajouter à la liste
+        $sousReseau = [SousReseau]::new(($i + 1), $NbHote, $AdresseCourante, $MasqueFin)
+        $sousReseaux += $sousReseau
+
+        # Calculer la capacité du sous-réseau (taille du bloc)
+        [uint32]$CapaciteSousReseau = [uint32][Math]::Pow(2, (32 - $MasqueFin))
+
+        # Avancer à la prochaine adresse réseau (séquentiel)
+        $base = $base + $CapaciteSousReseau
+
+         
+
+    }
+    
+           Write-Debug "Calculer() - fin"
+    
+    return $sousReseaux
+    }
+ 
+    
+  
 }
+ 
